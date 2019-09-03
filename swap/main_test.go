e1ec46ec58ad154f75fa609bedf9cd13b1826528
Wayne <waynejeng@aetheras.io>
2019-09-03 15:22:24 +0800

    [Change] Add swap function

---
+++ swap/main_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestSwap(t *testing.T) {
+	a := 3
+	b := 5
+	Swap(&a, &b)
+	expected := (a == 5 && b == 3)
+	if !expected {
+		t.Errorf("Swapping failed")
+	}
+}
