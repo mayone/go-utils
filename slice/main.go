e423af7a752dd49abbf7018d82ab90eac8a7ddc9
Wayne <waynejeng@aetheras.io>
2019-09-09 09:32:30 +0800

    [Change] Remove func main in slice

--- slice/main.go
+++ slice/main.go
@@ -1,15 +1,6 @@
 package main
 
-import "fmt"
-
 // RemoveByIndex will remove element at the specified index
 func RemoveByIndex(slice *[]int, index int) {
 	*slice = append((*slice)[:index], (*slice)[index+1:]...)
-}
-
-func main() {
-	sliceInMain := []int{5, 2, 3, 1, 4}
-	fmt.Println(sliceInMain)
-	RemoveByIndex(&sliceInMain, 1)
-	fmt.Println(sliceInMain)
 }
