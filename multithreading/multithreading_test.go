2a41279772cc7795965b92250d29af52b7db2b9b
Wayne <waynejeng@aetheras.io>
2020-02-26 16:02:52 +0800

    [Change] Add test cases

--- multithreading/multithreading_test.go
+++ multithreading/multithreading_test.go
@@ -6,10 +6,26 @@ import (
 )
 
 func TestAddArrays(t *testing.T) {
-	a := []int{1, 0, 2, 0, 3}
-	b := []int{-1, -1, -1, -1, -1}
+	a := []int{}
+	b := []int{}
 	expected := addArraysSequential(a, b)
 	result := addArraysParallel(a, b)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	}
+
+	a = []int{}
+	b = []int{-1, -1, -1, -1, -1}
+	expected = addArraysSequential(a, b)
+	result = addArraysParallel(a, b)
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("expected: %v, got: %v", expected, result)
+	}
+
+	a = []int{1, 0, 2, 0, 3}
+	b = []int{-1, -1, -1, -1, -1}
+	expected = addArraysSequential(a, b)
+	result = addArraysParallel(a, b)
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("expected: %v, got: %v", expected, result)
 	}
