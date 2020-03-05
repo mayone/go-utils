497f87b812cb1b2bd9c174f2431a51eac7d81c4c
Wayne <waynejeng@aetheras.io>
2020-03-05 17:40:11 +0800

    [Change] Use subtest

--- slice/slice.go
+++ slice/slice.go
@@ -1,6 +1,14 @@
 package slice
 
-// removeByIndex will remove element at the specified index
-func removeByIndex(slice *[]int, index int) {
-	*slice = append((*slice)[:index], (*slice)[index+1:]...)
+// removeByIndex will return slice with removal of element at the specified index
+func removeByIndex(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice
+	}
+	return append(slice[:index], slice[index+1:]...)
 }
+
+// removeByIndex will remove element at the specified index
+// func removeByIndex(slice *[]int, index int) {
+// 	*slice = append((*slice)[:index], (*slice)[index+1:]...)
+// }
--- slice/slice_test.go
+++ slice/slice_test.go
@@ -5,12 +5,64 @@ import (
 	"testing"
 )
 
-func TestRemoveByIndex(t *testing.T) {
-	slice := []int{1, 2, 3, 4, 5}
-	removeByIndex(&slice, 2)
-	expected := []int{1, 2, 4, 5}
-	result := slice
-	if !reflect.DeepEqual(result, expected) {
-		t.Errorf("expected: %v, got: %v", expected, result)
+// func TestRemoveByIndex(t *testing.T) {
+// 	slice := []int{1, 2, 3, 4, 5}
+// 	removeByIndex(&slice, 2)
+// 	expected := []int{1, 2, 4, 5}
+// 	result := slice
+// 	if !reflect.DeepEqual(result, expected) {
+// 		t.Errorf("expected: %v, got: %v", expected, result)
+// 	}
+// }
+
+func Test_removeByIndex(t *testing.T) {
+	type args struct {
+		slice []int
+		index int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Remove head",
+			args: args{
+				slice: []int{1, 2, 3, 4, 5},
+				index: 0,
+			},
+			want: []int{2, 3, 4, 5},
+		},
+		{
+			name: "Remove middle",
+			args: args{
+				slice: []int{1, 2, 3, 4, 5},
+				index: 2,
+			},
+			want: []int{1, 2, 4, 5},
+		},
+		{
+			name: "Remove tail",
+			args: args{
+				slice: []int{1, 2, 3, 4, 5},
+				index: 4,
+			},
+			want: []int{1, 2, 3, 4},
+		},
+		{
+			name: "Remove index out of bound",
+			args: args{
+				slice: []int{1, 2, 3, 4, 5},
+				index: 5,
+			},
+			want: []int{1, 2, 3, 4, 5},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := removeByIndex(tt.args.slice, tt.args.index); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("removeByIndex() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
