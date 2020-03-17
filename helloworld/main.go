deae2efec69f2343c6b788da6198a61dc639329b
Wayne <waynejeng@aetheras.io>
2020-03-17 14:59:35 +0800

    [Change] Update Makefile to show build.Info

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/mayone/Go-Utilities/helloworld/build"
 	"github.com/mayone/Go-Utilities/helloworld/greet"
 )
 
 func main() {
+	fmt.Println(build.Info())
 	fmt.Println(greet.Greet("Sweaty", "Sweety"))
 }
