dca9eaa8dfc22ca7633eb924f929952184f9bef9
Wayne <waynejeng@aetheras.io>
2020-03-17 13:55:29 +0800

    [Feature] Add package for build info

---
+++ helloworld/build/build.go
@@ -0,0 +1,27 @@
+package build
+
+import (
+	"fmt"
+)
+
+// These variables are set using -ldflags
+var (
+	name           string
+	version        string
+	gitBranch      string
+	lastCommitSHA  string
+	lastCommitTime string
+	goVersion      string
+)
+
+func BuildInfo() string {
+	return fmt.Sprintf(`
+Name             : %v
+Release          : %v
+Commit SHA1      : %v
+Commit Timestamp : %v
+Branch           : %v
+Go Version       : %v
+`,
+		name, version, lastCommitSHA, lastCommitTime, gitBranch, goVersion)
+}
