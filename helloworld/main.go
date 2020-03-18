de811d03107ac7e9731c1691ff9778d8785be531
Wayne <waynejeng@aetheras.io>
2020-03-18 12:08:23 +0800

    [Change] Rename package name to avoid confusion

--- helloworld/main.go
+++ helloworld/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/mayone/Go-Utilities/helloworld/build"
+	"github.com/mayone/Go-Utilities/helloworld/buildinfo"
 	"github.com/mayone/Go-Utilities/helloworld/greet"
 )
 
 func main() {
-	fmt.Println(build.Info())
+	fmt.Println(buildinfo.Info())
 	fmt.Println(greet.Greet("Sweaty", "Sweety"))
 }
