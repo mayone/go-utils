3db426009b2b84522e4fba747a399a77dc36d7b7
Wayne <waynejeng@aetheras.io>
2020-04-15 21:56:59 +0800

    [Change] Show config

--- helloworld/main.go
+++ helloworld/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 
 	"github.com/mayone/Go-Utilities/helloworld/buildinfo"
+	"github.com/mayone/Go-Utilities/helloworld/config"
 	"github.com/mayone/Go-Utilities/helloworld/greet"
 )
 
 func main() {
 	fmt.Println(buildinfo.Info())
+	fmt.Println(config.SayHello())
 	fmt.Println(greet.Greet("Sweaty", "Sweety"))
 }
